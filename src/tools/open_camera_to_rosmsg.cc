3b7556a2f314c486df276b3609fa855538d3426d
shun li <shun.li.at.casia@outlook.com>
2024-04-23 15:40:19 +0800

    u

--- src/tools/open_camera_to_rosmsg.cc
+++ src/tools/open_camera_to_rosmsg.cc
@@ -13,8 +13,57 @@
  *
  *******************************************************************************/
 
+#include "utility_tool/cmdline.h"
+#include "utility_tool/print_ctrl_macro.h"
+
 #include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
+#include <opencv2/opencv.hpp>
 
+int main(int argc, char** argv) {
+  cmdline::parser par;
 
+  par.add<int>("camera_id", 'i', "camera id", false, 0);
+  par.parse_check(argc, argv);
+
+  ros::init(argc, argv, "open_camera_to_rosmsg_node");
+  ros::NodeHandle nh;
+
+  int camera_id = par.get<int>("camera_id");
+  ros::Publisher image_pub = nh.advertise<sensor_msgs::Image>(
+      "image_raw_" + std::to_string(camera_id), 1);
+
+  cv::VideoCapture cap;
+  cap.open(camera_id, cv::CAP_V4L2);
+  cap.set(cv::CAP_PROP_CONVERT_RGB, 0);
+
+  cap.set(cv::CAP_PROP_FRAME_WIDTH, 3840);
+  cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
+  cap.set(cv::CAP_PROP_FPS, 30);
+
+  if (!cap.isOpened()) {
+    PCM_PRINT_ERROR("can not open the camera!\n");
+    return -1;
+  } else {
+    PCM_PRINT_INFO("open the camera successfully!\n");
+  }
+
+  while (ros::ok()) {
+    cv::Mat frame, rgb;
+    cap >> frame;
+    cv::cvtColor(frame, rgb, cv::COLOR_YUV2GRAY_UYVY);
+
+    // pub as ros msg
+    std_msgs::Header header;
+    header.frame_id = "camera_" + std::to_string(camera_id);
+    header.stamp = ros::Time::now();
+    sensor_msgs::ImagePtr msg =
+        cv_bridge::CvImage(header, "mono8", rgb).toImageMsg();
+    image_pub.publish(msg);
+  }
+
+  cap.release();
+  PCM_PRINT_INFO("the node is shutdown!\n");
+  return 0;
+}
