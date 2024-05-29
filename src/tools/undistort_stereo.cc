67391731427e308132bbe995d83d84e379c051e2
Shun Li <shun.li.at.casia@outlook.com>
2024-05-29 16:19:06 +0800

    update

---
+++ src/tools/undistort_stereo.cc
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ *   Copyright (C) 2023 CASIA. All rights reserved.
+ *
+ *   @Filename: undistort_stereo.cc
+ *
+ *   @Author: shun li
+ *
+ *   @Email: shun.li.at.casia@outlook.com
+ *
+ *   @Date: 29/05/2024
+ *
+ *   @Description: subscribe the hconcated raw, undistort, pub the hconcate_rect and
+ *   left_rect
+ *
+ *******************************************************************************/
+
+
+
