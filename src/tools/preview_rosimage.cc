b9d3141fdba27e80713f68f6b452715c35c23653
Shun Li <shun.li.at.casia@outlook.com>
2024-06-17 11:04:40 +0800

    update

---
+++ src/tools/preview_rosimage.cc
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ *   Copyright (C) 2023 CASIA. All rights reserved.
+ *
+ *   @Filename: preview_rosimage.cc
+ *
+ *   @Author: shun li
+ *
+ *   @Email: shun.li.at.casia@outlook.com
+ *
+ *   @Date: 17/06/2024
+ *
+ *   @Description:
+ *
+ *******************************************************************************/
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include <opencv2/highgui/highgui.hpp>
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+  try {
+    // Convert the ROS image message to OpenCV format using cv_bridge
+    cv::Mat cv_image =
+        cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8)->image;
+
+    // Calculate the new size, halving both width and height
+    cv::Size new_size(cv_image.cols / 2, cv_image.rows / 2);
+
+    // Resize the image
+    cv::Mat resized_image;
+    cv::resize(cv_image, resized_image, new_size, 0, 0,
+               cv::INTER_LINEAR);  // Use linear interpolation for resizing
+
+    // Display the resized image
+    cv::imshow("Resized Image Window", resized_image);
+    cv::waitKey(
+        30);  // Wait for 30ms to prevent the program from exiting immediately
+  } catch (cv_bridge::Exception& e) {
+    ROS_ERROR("Conversion from '%s' to 'bgr8' failed.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char** argv) {
+  // Initialize the ROS node
+  ros::init(argc, argv, "preview_rosimage_node");
+  ros::NodeHandle nh;
+
+  // Create an Image Transport object
+  image_transport::ImageTransport it(nh);
+
+  // Subscribe to the image topic, e.g., "/camera/image_raw"; adjust as needed
+  image_transport::Subscriber sub =
+      it.subscribe("/image_raw_0", 1, imageCallback);
+
+  // Spin to start the ROS event processing loop
+  ros::spin();
+
+  return 0;
+}
--- src/tools/split_hconcate_image.cc
+++ src/tools/split_hconcate_image.cc
@@ -26,6 +26,11 @@
 cv::Mat hconcate_img;
 std_msgs ::Header hconcate_img_header;
 
+image_transport::Publisher pub_left;
+image_transport::Publisher pub_right;
+
+int camera_id;
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
   try {
     hconcate_img = cv_bridge::toCvCopy(msg, "bgr8")->image;
@@ -35,13 +40,39 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
     PCM_PRINT_ERROR("Could not convert from '%s' to 'bgr8'. \n",
                     msg->encoding.c_str());
   }
+
+  int width = hconcate_img.cols;
+  int height = hconcate_img.rows;
+
+  cv::Rect leftRect(0, 0, width / 2, height);
+  cv::Rect rightRect(width / 2, 0, width / 2, height);
+
+  cv::Mat leftImage = hconcate_img(leftRect);
+  cv::Mat rightImage = hconcate_img(rightRect);
+
+  if (leftImage.empty() || rightImage.empty()) {
+    PCM_PRINT_WARN("the left or right image is empty! \n");
+    return;
+  }
+  std_msgs::Header l_header = hconcate_img_header;
+  l_header.frame_id = "cam_" + std::to_string(camera_id) + "_img_0";
+  std_msgs::Header r_header = hconcate_img_header;
+  r_header.frame_id = "cam_" + std::to_string(camera_id) + "_img_1";
+
+  sensor_msgs::ImagePtr left_msg =
+      cv_bridge::CvImage(l_header, "bgr8", leftImage).toImageMsg();
+  sensor_msgs::ImagePtr right_msg =
+      cv_bridge::CvImage(r_header, "bgr8", rightImage).toImageMsg();
+
+  pub_left.publish(left_msg);
+  pub_right.publish(right_msg);
 }
 
 int main(int argc, char** argv) {
   cmdline::parser par;
   par.add<int>("camera_id", 'i', "camera id", false, 0);
   par.parse_check(argc, argv);
-  int camera_id = par.get<int>("camera_id");
+  camera_id = par.get<int>("camera_id");
 
   ros::init(argc, argv,
             "split_hconcate_image_node_" + std::to_string(camera_id));
@@ -51,44 +82,9 @@ int main(int argc, char** argv) {
   image_transport::Subscriber sub = it.subscribe(
       "hconcate_image_cam_" + std::to_string(camera_id), 10, imageCallback);
 
-  image_transport::Publisher pub_left =
-      it.advertise("cam_" + std::to_string(camera_id) + "_img_0", 10);
-  image_transport::Publisher pub_right =
-      it.advertise("cam_" + std::to_string(camera_id) + "_img_1", 10);
+  pub_left = it.advertise("cam_" + std::to_string(camera_id) + "_img_0", 10);
+  pub_right = it.advertise("cam_" + std::to_string(camera_id) + "_img_1", 10);
 
-  while (ros::ok()) {
-    ros::spinOnce();
-    if (hconcate_img.empty()) {
-      PCM_PRINT_WARN("the hconcate image is empty! \n");
-      continue;
-    }
-
-    int width = hconcate_img.cols;
-    int height = hconcate_img.rows;
-
-    cv::Rect leftRect(0, 0, width / 2, height);
-    cv::Rect rightRect(width / 2, 0, width / 2, height);
-
-    cv::Mat leftImage = hconcate_img(leftRect);
-    cv::Mat rightImage = hconcate_img(rightRect);
-
-    if (leftImage.empty() || rightImage.empty()) {
-      PCM_PRINT_WARN("the left or right image is empty! \n");
-      continue;
-    }
-    std_msgs::Header l_header = hconcate_img_header;
-    l_header.frame_id = "cam_" + std::to_string(camera_id) + "_img_0";
-    std_msgs::Header r_header = hconcate_img_header;
-    r_header.frame_id = "cam_" + std::to_string(camera_id) + "_img_1";
-
-    sensor_msgs::ImagePtr left_msg =
-        cv_bridge::CvImage(l_header, "bgr8", leftImage).toImageMsg();
-    sensor_msgs::ImagePtr right_msg =
-        cv_bridge::CvImage(r_header, "bgr8", rightImage).toImageMsg();
-
-    pub_left.publish(left_msg);
-    pub_right.publish(right_msg);
-  }
-
+  ros::spin();
   return 0;
 }
