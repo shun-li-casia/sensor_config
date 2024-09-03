49713762b6b0498e1730275a1ebbf8808486d6d2
groundstation <shunli@casia>
2024-09-03 10:42:06 +0800

    update

--- src/tools/convert_kalibr_to_vins_param.cc
+++ src/tools/convert_kalibr_to_vins_param.cc
@@ -21,6 +21,7 @@ int main(int argc, char* argv[]) {
   par.add<int>("uav_id", 0, "uav id", true);
   par.add<std::string>("k_imu", 0, "kalibr imu file", true);
   par.add<std::string>("k_cam_imu", 0, "kalibr camera imu file", true);
+  par.add<float>("imu_noise_factor", 0, "imu noise factor", true, 1.0f);
   par.parse_check(argc, argv);
 
   std::string k_imu = par.get<std::string>("k_imu");
@@ -31,7 +32,7 @@ int main(int argc, char* argv[]) {
   sensor_config::StereoImu s(uav_id);
   s.readKalibr(k_cam_imu, k_imu);
   s.writeVins("uav_" + std::to_string(uav_id) + "_vins_params.yaml",
-              par.get<int>("uav_id"));
+              par.get<int>("uav_id"), par.get<float>("imu_noise_factor"));
 
   return 0;
 }
