94999e5c11de800be20d8437cd7712740a1f8030
uav_1 <uav_1@uav_1.com>
2024-09-07 10:12:36 +0800

    u

--- scripts/kalibr_usage.sh
+++ scripts/kalibr_usage.sh
@@ -1,3 +1,3 @@
 gnome-terminal --window  -e 'bash -c "taskset -c 2 roslaunch sensor_config open_camera_split_imu.launch uav_id:=$UAV_ID; exec bash"' \
-    --tab -e 'bash -c "source ~/.bashrc; sleep 5;rosrun sensor_config preview_rosimage -i /uav_${UAV_ID}/cam_0; exec bash"' \
-    --tab -e 'bash -c "source ~/.bashrc; sleep 5;rosrun sensor_config preview_rosimage -i /uav_${UAV_ID}/cam_1; exec bash"' \
+    --tab -e 'bash -c "source ~/.bashrc; sleep 5;taskset -c 7 rosrun sensor_config preview_rosimage -i /uav_${UAV_ID}/cam_0; exec bash"' \
+    --tab -e 'bash -c "source ~/.bashrc; sleep 6;taskset -c 7 rosrun sensor_config preview_rosimage -i /uav_${UAV_ID}/cam_1; exec bash"' \
