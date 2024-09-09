52955f5bda0f0dd1bcefb298f856110b94b683c5
shun li <is.shun.li@outlook.com>
2024-09-09 11:12:10 +0800

    add v4l2

---
+++ 3rdparty/libv4l2_opencv_mat/README.md
@@ -0,0 +1,44 @@
+
+libv4l2cpp
+====================
+
+It is a C++ wrapper for V4L2 mofified from  https://github.com/mpromonet/libv4l2cpp by mpromonet.
+I added more prints and solved  problem which is a video device may have rather than one video resource.And main keypoints I added was opencv Mat BGR format surport.
+
+Dependencies
+------------
+ - liblog4cpp5-dev (optional)
+ 
+V4L2 Capture
+-------------
+ - create a V4L2 Capture interface using MMAP interface:
+
+         V4L2DeviceParameters param("/dev/video0", V4L2_PIX_FMT_*, width, height, fps, input_index, verbose);
+         V4l2Capture* videoCapture = V4l2Capture::create(param, V4l2Access::IOTYPE_MMAP);
+
+ - data are available :
+
+         timeval timeout; 
+         bool isReadable = (videoCapture->isReadable(&timeout) == 1);
+
+ - read data :
+
+         size_t nb = videoCapture->read(buffer, bufferSize);
+
+
+V4L2 Output
+-------------
+
+ - To create a V4L2 Output interface using MMAP interface:
+
+         V4L2DeviceParameters param("/dev/video0", V4L2_PIX_FMT_*, width, height, fps, input_index , verbose);
+         V4l2Output* videoOutput = V4l2Output::create(param, V4l2Access::IOTYPE_MMAP);
+
+ - data could be written :
+
+         timeval timeout; 
+         bool isWritable = (videoOutput->isWritable(&timeout) == 1);
+
+ - write data :
+
+         size_t nb = videoOutput->write(buffer, bufferSize);
--- src/tools/open_camera_split_imu_to_rosmsg_v4l2.cc
+++ src/tools/open_camera_split_imu_to_rosmsg_v4l2.cc
@@ -1,14 +1,14 @@
 /*******************************************************************************
  *   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
  *
- *   @Filename: open_camera_spilt_imu_to_rosmsg_ros_time.cc
+ *   @Filename: open_camera_spilt_imu_to_rosmsg_v4l2.cc
  *
  *   @Author: Ting Cai, Shun Li
  *
  *
  *   @Date: 13/06/2024
  *
- *   @Description: BUG: cannot use ros::Time::now(), tp will be a mess!
+ *   @Description:
  *
  *******************************************************************************/
 
@@ -24,6 +24,7 @@
 #include "utility_tool/file_writter.h"
 #include "utility_tool/system_lib.h"
 #include "utility_tool/print_ctrl_macro.h"
+#include "utility_tool/pcm_debug_helper.h"
 #include <cmath>
 
 #include <ros/duration.h>
@@ -34,6 +35,9 @@
 #include <sensor_msgs/Imu.h>
 #include <opencv2/opencv.hpp>
 #include <mutex>
+
+#include <V4l2Capture.h>
+#include <V4l2Device.h>
 
 struct imu_data {
   float gyr_x_{0.0f};
@@ -48,25 +52,29 @@ struct imu_data {
 
 constexpr float g = 9.8015;
 constexpr float pi_div_180 = M_PI / 180.0f;
+// t_imu = t_cam + time_shift
+constexpr double time_shift = -0.036f;
+// NOTE: 48.6710
+constexpr double g_imu_t_step_s = 49.02 * 1e-6;
 
-// NOTE: t_imu = t_cam + time_shift
-// constexpr double time_shift = -0.036f;
-
-static bool g_imu_is_ready = false;
+std::atomic<bool> g_imu_is_ready;
 static unsigned int uart_baudrate = 1500000;
 
 ros::Publisher g_imu_pub;
+ros::Time g_time_start, g_imu_time;
+std::mutex g_imu_t_mutex;
+
+uint16_t g_last_stamp = UINT16_MAX, g_max_stamp = 0;
+bool g_is_first_frame = true;
 
 uint32_t g_imu_seq = 0, g_img_seq = 0;
 char* g_imu_env_value = NULL;
 utility_tool::FileWritter::Ptr g_imu_writter, g_img_writter;
 
 void ImuCallback(unsigned char* data_block, int data_block_len) {
-  if (!g_imu_is_ready) {
-    ros::Duration(0, 1e6).sleep();
+  if (!g_imu_is_ready.load()) {
     return;
   }
-
   struct imu_data data;
 
   if (g_imu_env_value != NULL) {
@@ -100,9 +108,47 @@ void ImuCallback(unsigned char* data_block, int data_block_len) {
   data.frame_id_ = (uint32_t)(data_block[18] | (data_block[19] << 8) |
                               (data_block[20] << 16) | (data_block[21] << 24));
 
+  if (g_is_first_frame) {
+    g_is_first_frame = false;
+    g_time_start = ros::Time::now();
+    g_imu_t_mutex.lock();
+    g_imu_time = g_time_start;
+    g_imu_t_mutex.unlock();
+  } else {
+    double time_diff_s = 0.0f;
+    if (data.stamp_ <= 812) {
+      // update the max stamp
+      if (data.stamp_ > g_max_stamp) {
+        g_max_stamp = data.stamp_;
+      }
+
+      if (data.stamp_ > g_last_stamp) {
+        uint16_t stamp_diff = data.stamp_ - g_last_stamp;
+        time_diff_s = stamp_diff * g_imu_t_step_s;
+      } else if (data.stamp_ < g_last_stamp) {
+        uint16_t stamp_diff = g_max_stamp - g_last_stamp + 10 + data.stamp_;
+        time_diff_s = stamp_diff * g_imu_t_step_s;
+      }
+
+      // update the last stamp
+      g_last_stamp = data.stamp_;
+    } else {
+      g_imu_t_mutex.lock();
+      g_imu_writter->Write(g_imu_time.toSec(), data.stamp_, time_diff_s,
+                           data.acc_x_, data.acc_y_, data.acc_z_, data.gyr_x_,
+                           data.gyr_y_, data.gyr_z_);
+      g_imu_t_mutex.unlock();
+      return;
+    }
+
+    g_imu_t_mutex.lock();
+    g_imu_time += ros::Duration(time_diff_s);
+    g_imu_t_mutex.unlock();
+  }
+
   sensor_msgs::Imu imu_msg;
   imu_msg.header.seq = data.frame_id_;
-  imu_msg.header.stamp = ros::Time::now();
+  imu_msg.header.stamp = g_imu_time;
   imu_msg.header.frame_id = "imu_raw_0";
   imu_msg.angular_velocity.x = data.gyr_x_;
   imu_msg.angular_velocity.y = data.gyr_y_;
@@ -129,6 +175,7 @@ int main(int argc, char* argv[]) {
   ros::NodeHandle nh;
 
   // init imu
+  g_imu_is_ready.store(false);
   g_imu_env_value = getenv("UAV_ID");
   g_imu_writter = std::make_shared<utility_tool::FileWritter>(
       "imu_debug_" + utility_tool::GetCurLocalTimeStr("%Y%m%d%H%M%S") + ".csv",
@@ -164,49 +211,56 @@ int main(int argc, char* argv[]) {
   g_imu_pub = nh.advertise<sensor_msgs::Imu>("imu_raw_0", 10000);
 
   // init the camera
-  cv::VideoCapture cap;
+  const std::string in_devname = "/dev/video" + std::to_string(camera_id);
+  V4L2DeviceParameters mparam(in_devname.c_str(), V4L2_PIX_FMT_UYVY, 3840, 1080,
+                              25, 0, 0);
+  V4l2Capture* videoCapture;
+
+  for (int i = 0; i < 10; ++i) {
+    videoCapture = V4l2Capture::create(mparam, V4l2Access::IOTYPE_MMAP);
+    if (!videoCapture) {
+      PCM_PRINT_ERROR("create videoCapture failed!\n");
+      ros::Duration(1).sleep();
+      continue;
+    } else {
+      PCM_PRINT_INFO("create videoCapture successfully!\n");
+      break;
+    }
+  }
+  timeval capture_time_out = {0, 400000};  // 40 millisecond
+
   g_img_writter = std::make_shared<utility_tool::FileWritter>(
       "img_debug_" + utility_tool::GetCurLocalTimeStr("%Y%m%d%H%M%S") + ".csv",
       6);
   g_img_writter->SetDelimiter(",");
   g_img_writter->EraseOpen();
 
-  // loop try to open the camera
-  for (int i = 0; i < 10; ++i) {
-    cap.open(camera_id, cv::CAP_V4L2);
-    bool is_open = cap.isOpened();
-    if (is_open) {
-      PCM_PRINT_INFO("open the camera successfully!\n");
-      break;
-    }
-    if (!is_open && i == 9) {
-      PCM_PRINT_ERROR("can not open the camera after %d times! Exit!\n", i + 1);
-      return -1;
-    }
-
-    PCM_PRINT_INFO("tryed %d times to open the camera.\n", i + 1);
-    ros::Duration(1).sleep();
-  }
-
-  cap.set(cv::CAP_PROP_CONVERT_RGB, 0);
-  cap.set(cv::CAP_PROP_FRAME_WIDTH, 3840);
-  cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
-  cap.set(cv::CAP_PROP_FPS, 25);
-
-  if (!cap.isOpened()) {
-    PCM_PRINT_ERROR("can not open the camera!\n");
-    return -1;
-  } else {
-    PCM_PRINT_INFO("open the camera successfully!\n");
-  }
-
   const int camera_is_stable = 10;
-  int count = 0;
+  uint32_t count = 0;
 
   ros::Time last_img_time(0);
-  while (ros::ok()) {
+  utility_tool::Timer t_cap, t_res;
+  while (true) {
     cv::Mat frame, raw_img;
-    cap >> frame;
+    t_cap.Start();
+    int ret = videoCapture->isReadable(&capture_time_out);
+    if (ret == -1) {
+      PCM_PRINT_ERROR("read frame error!\n");
+      // return -1;
+    } else if (ret == 0) {
+      PCM_PRINT_WARN("frame is not ready!\n");
+      ros::Duration(1).sleep();
+    } else {
+      ret = videoCapture->read(frame);
+      if (ret != 0) {
+        PCM_PRINT_ERROR("read frame error!\n");
+        // return -1;
+      }
+    }
+    float cap_time = t_cap.End() / 1000.0f;
+
+    t_res.Start();
+
     if (frame.empty()) {
       PCM_PRINT_WARN("frame is empty!\n");
       continue;
@@ -218,7 +272,9 @@ int main(int argc, char* argv[]) {
         PCM_PRINT_ERROR("can not open the serial %s", uart1.c_str());
         return -1;
       }
-      g_imu_is_ready = true;
+      g_imu_is_ready.store(true);
+      g_time_start = ros::Time::now();
+      g_imu_time = g_time_start;
       PCM_PRINT_INFO("start IMU!\n");
     }
 
@@ -240,7 +296,16 @@ int main(int argc, char* argv[]) {
     // pub as ros msg
     std_msgs::Header header;
     header.seq = g_img_seq++;
-    header.stamp = ros::Time::now();
+
+    float res_time = t_res.End() / 1000.0f;
+    // NOTE: compare the computer time and imu time
+    g_imu_t_mutex.lock();
+    if (g_imu_is_ready) {
+      header.stamp = g_imu_time + ros::Duration(time_shift);
+    } else {
+      header.stamp = ros::Time::now();
+    }
+    g_imu_t_mutex.unlock();
 
     std_msgs::Header l_header = header;
     l_header.frame_id = "uav_" + std::to_string(uav_id) + "_cam_0";
@@ -262,13 +327,27 @@ int main(int argc, char* argv[]) {
     }
     r_image_pub.publish(r_msg);
 
+    ros::Time time_now = ros::Time::now();
+    PCM_STREAM_DEBUG("image header tp: "
+                         << l_msg->header.stamp
+                         << " ros current time: " << time_now << " diff time: "
+                         << l_msg->header.stamp - time_now << std::endl;);
+
     PCM_PRINT_INFO("img tp: %lf, diff: %lf\n", l_msg->header.stamp.toSec(),
                    (l_msg->header.stamp - last_img_time).toSec());
 
+    g_img_writter->Write(ros::Time::now().toSec(),
+                         (l_msg->header.stamp - last_img_time).toSec(),
+                         cap_time, res_time / 1000.0f);
     last_img_time = l_msg->header.stamp;
   }
 
-  cap.release();
+  if (videoCapture->stop()) {
+    PCM_PRINT_INFO("stop video capture!\n");
+  } else {
+    PCM_PRINT_WARN("stop video failed!\n");
+  }
+  delete videoCapture;
 
   Serial_Device_UnInit();
   PCM_PRINT_INFO("the node is shutdown!\n");
