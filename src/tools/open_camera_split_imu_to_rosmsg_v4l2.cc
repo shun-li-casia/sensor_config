2e7eede54cff901682b7f9e71349b53ec2db65fe
uav_1 <uav_1@uav_1.com>
2024-09-12 13:56:49 +0800

    modify uav1 imu time step

--- src/tools/open_camera_split_imu_to_rosmsg_v4l2.cc
+++ src/tools/open_camera_split_imu_to_rosmsg_v4l2.cc
@@ -55,7 +55,7 @@ constexpr float pi_div_180 = M_PI / 180.0f;
 // t_imu = t_cam + time_shift
 constexpr double time_shift = -0.036f;
 // NOTE: 49.02
-constexpr double g_imu_t_step_table[] = {48.95905, 48.840219, 48.779830,
+constexpr double g_imu_t_step_table[] = {48.95905, 48.839846, 48.779830,
                                          48.780358, 48.7803955};
 std::atomic<double> g_imu_t_step_s;
 std::atomic<int> g_imu_cnt;
